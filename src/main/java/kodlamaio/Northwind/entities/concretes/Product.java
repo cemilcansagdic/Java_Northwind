a021d25993800627da032a5caf5726c93b7a864d
cemilcansagdic <cemilcansagdic@hotmail.com>
2021-06-02 14:28:30 +0300

    Commit

--- src/main/java/kodlamaio/Northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/Northwind/entities/concretes/Product.java
@@ -3,16 +3,17 @@ package kodlamaio.Northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 
 @Entity
-@Table(name="Product")
+@Table(name="products")
 public class Product {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "product_id")
 	private int id;
 	
