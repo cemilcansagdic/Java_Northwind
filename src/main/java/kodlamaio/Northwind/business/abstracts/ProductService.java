878708ad2727be46a422d70af95b2fddea053276
cemilcansagdic <cemilcansagdic@hotmail.com>
2021-06-05 19:01:16 +0300

    commit

--- src/main/java/kodlamaio/Northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/Northwind/business/abstracts/ProductService.java
@@ -7,6 +7,22 @@ import kodlamaio.Northwind.core.utilities.results.Result;
 import kodlamaio.Northwind.entities.concretes.Product;
 
 public interface ProductService {
+	
 	DataResult<List<Product>> getAll();
+	
 	Result add(Product product);
+
+	DataResult<Product> getByProductName(String productName);
+	
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int productId);
+	
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 }
